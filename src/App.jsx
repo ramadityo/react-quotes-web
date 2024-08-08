2e69bf1d089148c338ffcd1c6e097dbbe6cfa3a9
_userNull <ramzprivatexd@gmail.com>
2024-08-08 21:42:01 +0700

    fix bug animation + fetch error

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,51 @@
-import { useState, useRef, useEffect } from 'react'
-import {motion as m, AnimatePresence} from 'framer-motion'
-import './main.css'
-
+import { useState, useRef, useEffect } from "react";
+import { motion as m, AnimatePresence } from "framer-motion";
+import "./main.css";
 
 function App() {
-  // const quoteRef = useRef(null);
-  const [quote, setQuote] = useState('');
-  const [author, setAuthor] = useState('');
+    // const quoteRef = useRef(null);
+    const [quote, setQuote] = useState("");
+    const [author, setAuthor] = useState("");
+    const [quoteId, setQuoteId] = useState(0);
 
-  const randomizeQuote = () => {
-    fetch('https://api.quotable.io/random')
-      .then((res) => res.json())
-      .then((data) => {
-        setQuote(data.content);
-        setAuthor(data.author);
-      });
-  }
+    const randomizeQuote = () => {
+        fetch("https://dummyjson.com/quotes/random")
+            .then((res) => res.json())
+            .then((data) => {
+                setQuote(data.quote);
+                setAuthor(data.author);
+                setQuoteId(data.id);
+            });
+    };
 
-  useEffect(() => {
-    randomizeQuote();
-  }, []);
+    useEffect(() => {
+        randomizeQuote();
+    }, []);
 
-  const handleRandomQuote = () => {
-    randomizeQuote();
-  }
-  
+    const handleRandomQuote = () => {
+        randomizeQuote();
+    };
 
-  return (
-    <>
-      <div className="main-container">
-        <h1>React Quotes API</h1>
-        <AnimatePresence mode='wait'>
-          {quote && (
-            <m.div
-              key={quote.id}
-              initial={{ y: 30, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ y: 30, opacity: 0 }}
+    return (
+        <>
+            <div className="main-container">
+                <h1>React Quotes API</h1>
+                <AnimatePresence mode="wait">
+                    {quote && (
+                        <m.div key={quoteId} initial={{ y: 30, opacity: 0 }} animate={{ y: 0, opacity: 1 }} exit={{ y: 30, opacity: 0 }} className="quotes-box">
+                            <div className="quote">{quote}</div>
+                            <div className="author">{author}</div>
+                        </m.div>
+                    )}
+                </AnimatePresence>
 
-              className="quotes-box">
-              <div className="quote">{quote}</div>
-              <div className="author">{author}</div>
-            </m.div>
-          )}
-        </AnimatePresence>
-          
-        
-
-        <button className='randomizeBtn' onClick={handleRandomQuote}> Randomize quote! </button>
-      </div>
-    </>
-  )
+                <button className="randomizeBtn" onClick={handleRandomQuote}>
+                    {" "}
+                    Randomize quote!{" "}
+                </button>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
